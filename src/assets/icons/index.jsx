66ec970f4c90379ef5522ad88bc2ac2798619dd1
soumik183 <soumikbairagi213@gmail.com>
2025-11-07 21:31:38 +0530

    Add files via upload

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -379,3 +379,9 @@ export const Refresh = ({ className = "w-6 h-6" }) => (
     <path d="M20.49 9A9 9 0 005.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 013.51 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
+
+export const Power = ({ className = "w-6 h-6" }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18.36 6.64a9 9 0 11-12.73 0M12 2v10" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
